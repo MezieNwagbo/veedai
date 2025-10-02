e956071499b0e6822d9a9aeeaaf6bf756a03ad4d
Chimezie Nwagbo <chimezienwagbotech@gmail.com>
2025-10-02 08:13:32 +0100

    dashboard sidebar ui

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -239,7 +239,7 @@ export const SignUpView = () => {
             </form>
           </Form>
 
-          <div className="bg-radial from-blue-500 to-blue-600  relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="image" className="h-[92px] w-[92px]" />
             <p className="text-2xl font-semibold text-white">Veed.ai</p>
           </div>
